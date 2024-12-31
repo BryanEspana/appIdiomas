d2d2ff24783d1abeaf1ab8892063be6da557b76d
Bryan España <bryanespana21550@gmail.com>
2024-12-31 15:59:46 -0600

    Tema personalizable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import React, { useState } from 'react';
 import 'react-native-gesture-handler';
-import { MyTabs, Navigation } from './navigators/Navigation';
+import { MyTabs } from './navigators/Navigation';
+import { Button } from 'react-native'; // Para agregar un botón que cambie el tema
 
 export const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const theme = isDarkMode ? DarkTheme : DefaultTheme;
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={theme}>
       <MyTabs />
+      {/* Botón para cambiar el tema manualmente */}
+      {/*<Button title="Cambiar Tema" onPress={() => setIsDarkMode(!isDarkMode)} />*/}
     </NavigationContainer>
-  )
-}
+  );
+};
